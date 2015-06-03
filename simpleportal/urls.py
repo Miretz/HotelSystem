9c89e56e51fe512b8013b5d9d8be88d7e344120a
miretz <miretz@gmail.com>
2015-06-03 09:55:59 -0700

    redirect / to hotel

--- simpleportal/urls.py
+++ simpleportal/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+	url(r'^', include('hotel.urls')),
     url(r'^hotel/', include('hotel.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
